7ea26dfed9ae1a09ecb8542ed4cdae620ba9324b
Anna Ruiz <anna.ruiz@free.fr>
2025-09-04 12:23:56 +0200

    chore: front

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,32 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, signal } from '@angular/core';
+import { BookService } from './services/book.service';
+import { Book } from './models/book';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit{
   protected readonly title = signal('bookshelves-front');
+  public books?: Book[];
+
+  constructor(private bookService: BookService){}
+
+  ngOnInit(): void {
+      this.getBooks();
+  }
+
+  public getBooks(): void {
+    this.bookService.getBooks().subscribe({
+      next: (books: Book[]) => {
+        this.books = books;
+      },
+      error: (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    })
+  }
 }
---
+++ src/app/models/book.ts
@@ -0,0 +1,10 @@
+export interface Book {
+    id: number;
+    name: string;
+    description: string;
+    pages: number; 
+    author: string;
+    category: string;
+    imageUrl: string;
+    bookCode: string;
+}
